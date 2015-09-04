c16fc27b87bd532be1faa1e13a6c8fd7e4df789b
Zhao Wei Liew <zhaoweiliew@gmail.com>
2015-09-04 17:09:10 +0800

    hm2014811: Correct keylayout for hardware keys
    Change-Id: I8d316e1807f626d26a29b94f066439cd796faefd

--- device.mk
+++ device.mk
@@ -146,7 +146,7 @@ PRODUCT_COPY_FILES += \
 # Keylayout
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl \
-    $(LOCAL_PATH)/keylayout/synaptics_rmi4_i2c.kl:system/usr/keylayout/synaptics_rmi4_i2c.kl
+    $(LOCAL_PATH)/keylayout/ft5x06_ts.kl:system/usr/keylayout/ft5x06_ts.kl
 
 # Keystore
 PRODUCT_PACKAGES += \
