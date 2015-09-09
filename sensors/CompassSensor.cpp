432409dea0c3aa5d4ffdf76853d2277d2e42f3a3
Zhao Wei Liew <zhaoweiliew@gmail.com>
2015-09-09 08:37:22 +0800

    hm2014811: Update sensors HAL
    * Supports newer Lollipop sensor functions such as batching and flushing and a
    new pocket sensor based on proximity and light sensors.
    * Also fixes minimum poll delay calculation and reverts the previous revert I did
    since kernel has support for boot timestamps now.
    * CAF tag: LA.BR.1.2.4_rb1.16
    
    Change-Id: I7ab59904af9ae47413520b9449faaaa4e150bdc1

--- sensors/CompassSensor.cpp
+++ sensors/CompassSensor.cpp
@@ -57,7 +57,6 @@ ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 /*****************************************************************************/
 CompassSensor::CompassSensor(struct SensorContext *context)
 	: SensorBase(NULL, NULL, context),
-	  mEnabled(0),
 	  mInputReader(4),
 	  mHasPendingEvent(false),
 	  mEnabledTime(0),
@@ -125,7 +124,7 @@ int CompassSensor::enable(int32_t, int en) {
 }
 
 bool CompassSensor::hasPendingEvents() const {
-	return mHasPendingEvent;
+	return mHasPendingEvent || mHasPendingMetadata;
 }
 
 int CompassSensor::setDelay(int32_t, int64_t delay_ns)
@@ -146,7 +145,7 @@ int CompassSensor::setDelay(int32_t, int64_t delay_ns)
 	fd = open(input_sysfs_path, O_RDWR);
 	if (fd >= 0) {
 		char buf[80];
-		sprintf(buf, "%d", delay_ms);
+		snprintf(buf, sizeof(buf), "%d", delay_ms);
 		write(fd, buf, strlen(buf)+1);
 		close(fd);
 		return 0;
@@ -163,6 +162,13 @@ int CompassSensor::readEvents(sensors_event_t* data, int count)
 		mHasPendingEvent = false;
 		mPendingEvent.timestamp = getTimestamp();
 		*data = mPendingEvent;
+		return mEnabled ? 1 : 0;
+	}
+
+	if (mHasPendingMetadata) {
+		mHasPendingMetadata--;
+		meta_data.timestamp = getTimestamp();
+		*data = meta_data;
 		return mEnabled ? 1 : 0;
 	}
 
@@ -199,49 +205,48 @@ again:
 					mPendingEvent.timestamp = report_time+event->value;
 					break;
 				case SYN_REPORT:
-					mPendingEvent.timestamp = timevalToNano(event->time);
-					if (mEnabled) {
-						if (mPendingEvent.timestamp >= mEnabledTime) {
-							raw = mPendingEvent;
-
-							if (algo != NULL) {
-								if (algo->methods->convert(&raw, &result, NULL)) {
-									ALOGE("Calibration failed.");
-									result.magnetic.x = CALIBRATE_ERROR_MAGIC;
-									result.magnetic.y = CALIBRATE_ERROR_MAGIC;
-									result.magnetic.z = CALIBRATE_ERROR_MAGIC;
-									result.magnetic.status = 0;
-								}
-							} else {
-								result = raw;
-							}
-
-							*data = result;
-							data->version = sizeof(sensors_event_t);
-							data->sensor = mPendingEvent.sensor;
-							data->type = SENSOR_TYPE_MAGNETIC_FIELD;
-							data->timestamp = mPendingEvent.timestamp;
-
-							/* The raw data is stored inside sensors_event_t.data after
-							 * sensors_event_t.magnetic. Notice that the raw data is
-							 * required to composite the virtual sensor uncalibrated
-							 * magnetic field sensor.
-							 *
-							 * data[0~2]: calibrated magnetic field data.
-							 * data[3]: magnetic field data accuracy.
-							 * data[4~6]: uncalibrated magnetic field data.
-							 */
-							data->data[4] = mPendingEvent.data[0];
-							data->data[5] = mPendingEvent.data[1];
-							data->data[6] = mPendingEvent.data[2];
-
-							data++;
-							numEventReceived++;
-						}
-						count--;
-						break;
+					if (mUseAbsTimeStamp != true) {
+						mPendingEvent.timestamp = timevalToNano(event->time);
 					}
+					if (mEnabled) {
+						raw = mPendingEvent;
 
+						if (algo != NULL) {
+							if (algo->methods->convert(&raw, &result, NULL)) {
+								ALOGE("Calibration failed.");
+								result.magnetic.x = CALIBRATE_ERROR_MAGIC;
+								result.magnetic.y = CALIBRATE_ERROR_MAGIC;
+								result.magnetic.z = CALIBRATE_ERROR_MAGIC;
+								result.magnetic.status = 0;
+							}
+						} else {
+							result = raw;
+						}
+
+						*data = result;
+						data->version = sizeof(sensors_event_t);
+						data->sensor = mPendingEvent.sensor;
+						data->type = SENSOR_TYPE_MAGNETIC_FIELD;
+						data->timestamp = mPendingEvent.timestamp;
+
+						/* The raw data is stored inside sensors_event_t.data after
+						 * sensors_event_t.magnetic. Notice that the raw data is
+						 * required to composite the virtual sensor uncalibrated
+						 * magnetic field sensor.
+						 *
+						 * data[0~2]: calibrated magnetic field data.
+						 * data[3]: magnetic field data accuracy.
+						 * data[4~6]: uncalibrated magnetic field data.
+						 */
+						data->data[4] = mPendingEvent.data[0];
+						data->data[5] = mPendingEvent.data[1];
+						data->data[6] = mPendingEvent.data[2];
+
+						data++;
+						numEventReceived++;
+						count--;
+					}
+					break;
 			}
 		} else {
 			ALOGE("CompassSensor: unknown event (type=%d, code=%d)",
